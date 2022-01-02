ccb53c12fb41da953f00ba5a2a3769dfa1d66603
jjmerits <35485369+jjmerits@users.noreply.github.com>
2022-01-03 08:37:09 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,215 @@ import gnews
 import pytz
 import time
 
+#url = 'https://raw.githubusercontent.com/jjmerits/Dashboard/main/test_df_all.csv'
+#test_df = pd.read_csv(url)
 
+#name_list = test_df['Name'].unique().tolist()
+#df = test_df[test_df['Name'] == name_list[4]]
+
+
+#df['date'] = df['date'].apply(lambda x: datetime.strptime(x,'%Y-%m-%d %H:%M:%S').strftime('%Y/%m/%d'))
+#df.set_index("date", inplace = True)
+
+
+st.set_page_config(layout='wide')
+#fig = go.Figure()
+#fig.add_trace(go.Bar(x=df[['actual']].index,y=df['actual'].to_list(),name='actual'))
+#fig.add_trace(go.Bar(x=df[['actual']].index,y=df['forecast'].to_list(),name='forecast'))
+#fig.update_layout(barmode='group',title=name_list[4], yaxis=dict(title = 'y/y %'))
+
+#st.plotly_chart(fig,use_container_width=True)
+
+# Initialize connection.
+conn = pymongo.MongoClient(st.secrets.db_credentials.HOST,st.secrets.db_credentials.PORT, username=st.secrets.db_credentials.DB_USER,password=st.secrets.db_credentials.DB_TOKEN, tls=True, tlsAllowInvalidCertificates=True)
+
+# Pull data from the collection.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+event_list = conn.econdata.glob.find({}).distinct('event')
+cursor = conn.econdata.glob.find({'event':{"$in":event_list}},{'_id':False})
+df_all=pd.DataFrame(cursor)
+
+df_all = df_all.loc[df_all['date'].between('2017-01-01','2100-12-31', inclusive=False)]
+
+df_all['date'] = df_all['date'].apply(lambda x: datetime.strftime(x,'%Y/%m/%d'))
+#df.set_index("date", inplace = True)
+
+
+#plot graph
+def plot_graph(x,y=""):
+  df_x = df[(df['event'] == x) | (df['event'] == y)]
+  df_x = df_x.loc[df_x[['date','actual','forecast']].drop_duplicates().index]
+  df_x.sort_values('date', inplace = True)
+  df_x.set_index("date", inplace = True)
+  fig = go.Figure()
+  fig.add_trace(go.Bar(x=df_x[['actual']].index,y=df_x['actual'].to_list(),name='actual'))
+  fig.add_trace(go.Bar(x=df_x[['actual']].index,y=df_x['forecast'].to_list(),name='forecast'))
+  fig.update_layout(barmode='group',title=x, yaxis=dict(title = '')) #title=x+" "+df_x['currency'].values[1]
+  #fig.show()
+  st.plotly_chart(fig,use_container_width=True)
+
+st.header("PMI")
+st.write("US")
+df = df_all[df_all['currency'] == 'USD']
+
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("ISM Manufacturing PMI")
+
+with col2:
+  plot_graph("ISM Non-Manufacturing PMI","ISM Services PMI")
+##########################    
+
+st.write("EU")
+df = df_all[df_all['currency'] == 'EUR']
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("Flash Manufacturing PMI")
+
+with col2:
+  plot_graph("Flash Services PMI")
+  
+##########################  
+
+st.write("UK")
+df = df_all[df_all['currency'] == 'GBP']
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("Flash Manufacturing PMI")
+
+with col2:
+  plot_graph("Flash Services PMI")
+  
+##########################  
+st.write("China")
+df = df_all[df_all['currency'] == 'CNY']
+
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("Fixed Asset Investment ytd/y")
+
+with col2:
+  plot_graph("Industrial Production y/y")
+
+  ##########################  
+st.write("Aus")
+df = df_all[df_all['currency'] == 'AUD']
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("Flash Manufacturing PMI")
+
+with col2:
+  plot_graph("Flash Services PMI")
+########################################################################################
+
+st.header("CPI/PPI")
+st.write("US")
+df = df_all[df_all['currency'] == 'USD']
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("Core CPI m/m")
+
+with col2:
+  plot_graph("Core PPI m/m")
+##########################  
+st.write("EU")
+df = df_all[df_all['currency'] == 'EUR']
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("Final Core CPI y/y")
+
+with col2:
+  plot_graph("PPI m/m")
+  
+##########################    
+st.write("UK")
+df = df_all[df_all['currency'] == 'GBP']
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("Core CPI y/y")
+
+with col2:
+  plot_graph("Consumer Inflation Expectations")
+  
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("PPI Input m/m")
+
+with col2:
+  plot_graph("PPI Output m/m")
+    
+
+##########################  
+st.write("China")
+df = df_all[df_all['currency'] == 'CNY']
+
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("PPI y/y")
+
+with col2:
+  plot_graph("CPI y/y") 
+##########################  
+
+st.write("Aus")
+df = df_all[df_all['currency'] == 'AUD']
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("PPI q/q")
+
+with col2:
+  plot_graph("CPI q/q")
+  
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("Commodity Prices y/y")
+
+with col2:
+  plot_graph("Company Operating Profits q/q")
+###############################################################################################  
+st.header("Retail/Consumer")
+
+st.write("US")
+df =df_all[df_all['currency'] == 'USD']
+
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("Core Retail Sales m/m")
+
+with col2:
+  plot_graph("Non-Farm Employment Change")
+##########################  
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("Wards Total Vehicle Sales")
+
+with col2:
+  plot_graph("CB Consumer Confidence")
+##########################  
+st.write("EU")
+df =df_all[df_all['currency'] == 'EUR']
+
+col1, col2 = st.columns(2)
+
+with col1:
+  plot_graph("Retail Sales m/m")
+
+with col2:
+  plot_graph("Consumer Confidence")
 ########################## #############################################################
 
 def tz_diff(date, tz1, tz2):
@@ -81,14 +289,14 @@ def gnews_html(q_str, cn='US', la='english'):
 ##########################  
 col1, col2 = st.columns(2)
 with col1:
-  gnews_html("Tesla TLSA","US")
+  gnews_html("Dollar Index","US")
   
 with col2:
-  gnews_html("Devon dvn")
+  gnews_html("EUR currency")
 ##########################
 col1, col2 = st.columns(2)
 with col1:
-  gnews_html("Facebook FB","US")
+  gnews_html("GBP","GB")
   
 with col2:
   gnews_html("AUD currency","AU")
@@ -104,10 +312,10 @@ st.header(" ")
 ts = time.time()
 numofweek = str(datetime.fromtimestamp(ts).isocalendar()[1])
 numofyear = str(datetime.fromtimestamp(ts).isocalendar()[0])
-#url = f'https://raw.githubusercontent.com/jjmerits/Dashboard/main/01010{numofweek}{numofyear}final.HTML'
+url = f'https://raw.githubusercontent.com/jjmerits/Dashboard/main/01010{numofweek}{numofyear}final.HTML'
 #with open(url) as f:
-#t = requests.get(url,verify=False)
-#st.markdown(t.text, unsafe_allow_html=True)
+t = requests.get(url,verify=False)
+st.markdown(t.text, unsafe_allow_html=True)
   
 ##########################
 
